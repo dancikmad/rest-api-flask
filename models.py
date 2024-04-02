a446444103b4f61720a6b47577fbda5da685df64
Daniel Crigan <crgdaniel@yahoo.com>
2024-04-02 14:29:51 +0100

    Feed the database

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from config import db, ma
 class Note(db.Model):
     __tablename__ = "note"
     id = db.Column(db.Integer, primary_key=True)
-    person_id = db.Column(db.Integer, db.ForeignKey("person.d"))
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
     content = db.Column(db.String, nullable=False)
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
