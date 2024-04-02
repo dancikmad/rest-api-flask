4df0f153de0c241d3f770620abe4372d9bf2d057
Daniel Crigan <crgdaniel@yahoo.com>
2024-04-02 15:57:00 +0100

    Refactor adding helper module for db init

--- build_database.py
+++ build_database.py
@@ -9,6 +9,8 @@
 """
 
 from datetime import datetime
+from sqlalchemy.exc import OperationalError
+
 from config import app, db
 from models import Person, Note
 
@@ -41,8 +43,17 @@ PEOPLE_NOTES = [
     },
 ]
 
-with app.app_context():
-    db.drop_all()
+
+def get_data_from_table(model):
+    try:
+        data = db.session.query(model).all()
+        db.session.close()
+        return data
+    except OperationalError:
+        return []
+
+
+def create_database(db):
     db.create_all()
     for data in PEOPLE_NOTES:
         new_person = Person(lname=data.get("lname"), fname=data.get("fname"))
@@ -50,9 +61,32 @@ with app.app_context():
             new_person.notes.append(
                 Note(
                     content=content,
-                    timestamp=datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S"),
-
+                    timestamp=datetime.strptime(
+                        timestamp, "%Y-%m-%d %H:%M:%S"
+                    ),
                 )
             )
         db.session.add(new_person)
     db.session.commit()
+    print("Created new database")
+
+
+def update_database(db, existing_people, existing_notes):
+    db.drop_all()
+    db.create_all()
+    for person in existing_people:
+        db.session.merge(person)
+    for note in existing_notes:
+        db.session.merge(note)
+    db.session.commit()
+    print("Updated existing database")
+
+
+with app.app_context():
+    existing_people = get_data_from_table(Person)
+    existing_notes = get_data_from_table(Note)
+
+    if not existing_people:
+        create_database(db)
+    else:
+        update_database(db, existing_people, existing_notes)
