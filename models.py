85b6bebda9a7edeb026cd7c054c36f65ca43fb52
Daniel Crigan <crgdaniel@yahoo.com>
2024-04-02 12:47:59 +0100

    Create models file for the data in person db table

---
+++ models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from config import db
+
+class Person(db.Model):
+    __tablename__ = "person"
+    id = db.Column(db.Integer, primary_key=True)
+    lname = db.Column(db.String(32), uniqute=True)
+    fname = db.Column(db.String(32))
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    
